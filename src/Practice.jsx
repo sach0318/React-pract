498bf6419f4e0be2d93099026f9f735d307139a6
sach0318 <sachinyadv0318@gmail.com>
2025-06-23 12:17:29 +0530

    grid change

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -3,7 +3,7 @@ import SeriesCard from "./components/SeriesCard";
 
 export const Practice = () => {
   return (
-    <ul className=" grid grid-cols-2  px-2 py-10 mx-50 text-center gap-2 font-serif rounded-lg  ">
+    <ul className=" grid grid-cols-2  px-2 py-10 mx-50 text-center gap-5 font-serif rounded-lg  ">
       {sData.map((c) => {
         return <SeriesCard key={c.id} ele={c} />;
       })}
--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -2,16 +2,15 @@ const SeriesCard = ({ ele }) => {
   return (
     <li key={ele.key}>
       <div className=" ">
-        <img src={ele.img_url} alt="logo" className=" " />
+        <img src={ele.img_url} alt="logo" className="rounded-lg " />
         <h2>Name : {ele.name}</h2>
         <h3>Rating : {ele.rating} </h3>
         <h4 className="mx-auto ">Summary : {ele.description} </h4>
         <p>Genre: {ele.genre} </p>
         <p>Cast : {ele.cast} </p>
         <a href={ele.watch_url} target="_blank">
-          <button className=" border-2 bg-red-700 cursor-pointer ">
-            {" "}
-            Watch Now{" "}
+          <button className=" border-2 bg-lime-500 font-bold py-1 cursor-pointer rounded-lg p-1 ">
+            Watch Now
           </button>
         </a>
       </div>
